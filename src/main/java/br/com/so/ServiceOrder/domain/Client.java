d08f3c646bb16a224fb81495de97d01a115d0625
Cristyan de Souza <cristyansouzas2@gmail.com>
2022-11-20 20:21:21 -0300

    Creating domain classes

---
+++ src/main/java/br/com/so/ServiceOrder/domain/Client.java
@@ -0,0 +1,15 @@
+package br.com.so.ServiceOrder.domain;
+
+
+public class Client extends Person{
+
+	public Client() {
+		super();
+	}
+
+	public Client(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+	
+}
---
+++ src/main/java/br/com/so/ServiceOrder/domain/Technician.java
@@ -0,0 +1,13 @@
+package br.com.so.ServiceOrder.domain;
+
+public class Technician extends Person {
+
+	public Technician() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Technician(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
